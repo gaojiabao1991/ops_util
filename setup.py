ea4758fa9ea6cc0b92ae9ef2d58d0b3722214dc3
jiabao.gao <jiabao.gao@zhaopin.com.cn>
2025-01-15 18:49:48 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ops-util',
-    version='0.1.0',
+    version='0.1.1',
     packages=['ops_util'],
     install_requires=[],
     author='JiaBao Gao',
